9cd3b106f2747e70b435f453f1074fb02cdd917c
Yash Israni <yashisrani52@gmail.com>
2024-10-08 18:08:36 +0530

    Auth0 implementation

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+function App() {
+  const {loginWithRedirect,isAuthenticated,logout} = useAuth0();
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      {
+        isAuthenticated ? <button onClick={()=>logout()}>Logout</button> :  <button onClick={()=> loginWithRedirect()}>Login with Redireact</button>
+      }
+     
+      <div className="card">
+        
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Auth0Provider
+    domain="<Add your Auth0 domain>"
+    clientId="<Add your Auth0 clientID>"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+    <App />
+    </Auth0Provider>
+   
+  </StrictMode>,
+)
